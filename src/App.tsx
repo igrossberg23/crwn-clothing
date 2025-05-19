a210b49216203802d4a85b3efe9697b783611e51
igrossberg23 <igrossberg23@gmail.com>
2025-05-19 07:53:01 -0700

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import CategoriesContainer from './components/CategoriesContainer/CategoriesContainer';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Home from './routes/home/Home';
+import Navigation from './routes/navigation/Navigation';
+import SignIn from './routes/sign-in/SignIn';
+
+const Shop = () => {
+	return (
+		<div>
+			<p>I am the shop</p>
+		</div>
+	);
+};
 
 function App() {
-	const categories = [
-		{
-			id: 1,
-			title: 'hats',
-			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		},
-		{
-			id: 2,
-			title: 'jackets',
-			imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-		},
-		{
-			id: 3,
-			title: 'sneakers',
-			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-		},
-		{
-			id: 4,
-			title: 'womens',
-			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		},
-		{
-			id: 5,
-			title: 'mens',
-			imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-		},
-	];
-
 	return (
 		<div className='app'>
-			<CategoriesContainer categories={categories} />
+			<Routes>
+				<Route
+					path={'/'}
+					element={<Navigation />}>
+					<Route
+						index
+						element={<Home />}
+					/>
+					<Route
+						path='sign-in'
+						element={<SignIn />}
+					/>
+					<Route
+						path='shop'
+						element={<Shop />}
+					/>
+				</Route>
+			</Routes>
 		</div>
 	);
 }
---
+++ src/routes/navigation/Navigation.tsx
@@ -0,0 +1,35 @@
+import { Outlet, Link } from 'react-router-dom';
+import './Navigation.scss';
+import CrwnLogo from '../../assets/crown.svg';
+
+const Navigation = () => {
+	return (
+		<>
+			<div className='navigation'>
+				<Link
+					className='logo-container'
+					to='/'>
+					<img
+						src={CrwnLogo}
+						alt='Crwn Logo'
+					/>
+				</Link>
+				<div className='nav-links-container'>
+					<Link
+						className='nav-link'
+						to='/shop'>
+						Shop
+					</Link>
+					<Link
+						className='nav-link'
+						to='/sign-in'>
+						Sign In
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default Navigation;
